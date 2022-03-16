0526572e89e4f5b639feb374b808a1e7a9393fc0
Sboonny <88248797+Sboonny@users.noreply.github.com>
2022-03-16 21:41:27 +0200

    finished the challenge

--- data.js
+++ data.js
@@ -1,10 +1,11 @@
 const slipAdvice = document.querySelector("p")
 const slipId = document.querySelector("h1")
 
-async function getAdvice(ADVICE_API) {
+async function getAdvice() {
+  const ADVICE_API =  "https://api.adviceslip.com/advice"
   const adviceData = await fetch(ADVICE_API)
   const slipContainer = await adviceData.json()
   slipId.textContent= `Advice ${slipContainer.slip.id}`
   slipAdvice.textContent= `${slipContainer.slip.advice}`
 }
-getAdvice( "https://api.adviceslip.com/advice")
+getAdvice()
